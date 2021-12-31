a70922a296308dfff2776da1714fe20ede2556f3
Maya Comfort <mdcomfort@gmail.com>
2021-12-30 22:50:07 -0500

    complete except .toString() addition

---
+++ Main.java
@@ -0,0 +1,42 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Instantiate objects here!");
+
+		MelonType muskMelon = new MelonType("musk", "Muskmelon", 1998, "green", true, true);
+		MelonType casaba = new MelonType("cas", "Casaba", 2003, "orange", false, false);
+		MelonType crenshaw = new MelonType("cren", "Crenshaw", 1996, "green", false, false);
+		MelonType yellowWatermelon = new MelonType("yw", "Yellow Watermelon", 2013, "yellow", false, true);
+
+		MelonType[] melonTypes = new MelonType[4];
+		melonTypes[0] = muskMelon;
+		melonTypes[1] = casaba;
+		melonTypes[2] = crenshaw;
+		melonTypes[3] = yellowWatermelon;
+		System.out.println(melonTypes);
+
+		Melon melon1 = new Melon(melonTypes[3], 8, 7, 2, "Sheila");
+		Melon melon2 = new Melon(melonTypes[3], 3, 4, 2, "Sheila");
+		Melon melon3 = new Melon(melonTypes[1], 10, 6, 35, "Sheila");
+		Melon melon4 = new Melon(melonTypes[2], 8, 9,35, "Michael");
+		Melon melon5 = new Melon(melonTypes[2], 8, 2, 35, "Michael");
+		Melon melon6 = new Melon(melonTypes[0], 6, 7, 4, "Michael");
+		Melon melon7 = new Melon(melonTypes[3], 7, 10, 3, "Sheila");
+
+		Melon[] melons = { melon1, melon2, melon3, melon4, melon5, melon6, melon7 };
+		System.out.println(melons);
+
+		getSellabilityReport(melons);
+	}
+
+	public static void getSellabilityReport(Melon[] melons) {
+		for (int i = 0; i < melons.length; i++) {
+			if (melons[i].isSellable()) {
+				System.out.println("Harvested by " + melons[i].harvester + " from FIELD " + melons[i].field + " (CAN BE SOLD)");
+			}
+			else {
+				System.out.println("Harvested by " + melons[i].harvester + " from FIELD " + melons[i].field + " (NOT SELLABLE)");
+			}
+		}
+	}
+}
